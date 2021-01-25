ef6d8502ce59a5748f6086d31ef6e55c3c488476
Joel Cox <joelcox518@gmail.com>
2021-01-25 16:17:07 -0500

    html work

---
+++ data.py
@@ -0,0 +1,13 @@
+def get_db_data():
+    # Connect to database
+
+    # Query data
+
+    # Return data in json format
+    pass
+
+def get_api_data():
+    # Query to api
+
+    # Return data in json format
+    pass
