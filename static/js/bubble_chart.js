9e4764e80ab5e7e885fce8d106f2431bf1b5105d
doublealogic <cras.ajrobertson@gmail.com>
2021-01-27 20:05:56 -0500

    Added xText and yTextRefresh functions

--- static/js/bubble_chart.js
+++ static/js/bubble_chart.js
@@ -25,3 +25,45 @@ var svg = d3
     .attr("width", width)
     .attr("height", height)
     .attr("class", "chart");
+
+// Sets up the radius for each bubble in the graph.
+var bubbleRadius;
+function bblGet() {
+    if (width <= 530) {
+        bubbleRadius = 5;
+    }
+    else {
+        bubbleRadius = 10;
+    }
+}
+bblGet();
+
+// Axes Labels
+
+// Creates group element to nest the bottom axis labels
+svg.append("g").attr("class", "xText");
+
+var xText = d3.select(".xText");
+
+let botTxtX = (width - wordLabelArea) / 2 + wordLabelArea;
+let botTxtY = (height - margin - txtPadBottom);
+
+function xTextRefresh() {
+    xText.attr(
+        "transform",
+        "translate(" +
+            botTxtX + ", " + botTxtY + ")"
+    );
+}
+xTextRefresh();
+
+let leftTxtX = margin + txtPadBottom;
+let leftTxtY = (height + wordLabelArea) / 2 - wordLabelArea;
+
+function yTextRefresh() {
+    yText.attr(
+        "transform",
+        "translate(" + leftTxtX + ", " + leftTxtY + ")rotate(-90)"
+    );
+  }
+  yTextRefresh();
