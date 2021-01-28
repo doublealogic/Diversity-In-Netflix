cd240f649cad0ad0ebb58074f68fca4cd99b100b
doublealogic <cras.ajrobertson@gmail.com>
2021-01-27 19:43:48 -0500

    Updated bubble_chart.js with Plot Setup

--- static/js/bubble_chart.js
+++ static/js/bubble_chart.js
@@ -4,3 +4,24 @@
 
 // Grabs width of the box container
 var width = parseInt(d3.select("#bubble").style("width"));
+
+// Determines height of the graph
+var height = width - width / 3.9;
+
+// Defines margin spacing for graph
+var margin = 20;
+
+// Placeholder for word labels
+var wordLabelArea = 110;
+
+// Adds padding for bottom and left axes label texts
+var txtPadBottom = 40;
+var txtPadLeft = 40;
+
+// Creates graph canvas
+var svg = d3
+    .select("#bubble")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("class", "chart");
